46fc37acccdde887e06602c72cc0605fb3dea8f2
Slownik1 <76266435+Slownik1@users.noreply.github.com>
2021-04-13 15:37:52 +0200

    Add files via upload

---
+++ TabNr9/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TabNr9
+{
+    public struct KandydatNaStudia
+    {
+
+        public String nazwiskoKandydata;
+        public String imieKandydata;
+        public int procMatematyka;
+        public int procInformatyka;
+        public int procJezykObcy;
+
+        KandydatNaStudia(String nazwiskoKandydata, String imieKandydata, int procMatematyka, int procInformatyka, int procJezykObcy)
+        {
+            this.nazwiskoKandydata = nazwiskoKandydata;
+            this.imieKandydata = imieKandydata;
+            this.procMatematyka = procMatematyka;
+            this.procInformatyka = procInformatyka;
+            this.procJezykObcy = procJezykObcy;
+        }
+
+        public double Oblicz()
+        {
+
+            double punktyMatematyka = procMatematyka*0.6;
+            double punktyInformatyka = procInformatyka*0.5;
+            double punktyJezykObcy = procJezykObcy*0.2;
+
+            return procInformatyka + procJezykObcy + punktyMatematyka;
+
+        }
+
+    }
+}
---
+++ TabNr9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TabNr9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            KandydatNaStudia[] kandydat = new KandydatNaStudia[11];
+
+            kandydat[0].nazwiskoKandydata ="Krawczyk";
+            kandydat[1].imieKandydata = "Adrian";
+            kandydat[2].procMatematyka = 71;
+            kandydat[3].procInformatyka = 89;
+            kandydat[5].procJezykObcy = 90;
+
+            kandydat[6].nazwiskoKandydata = "Stenacel";
+            kandydat[7].imieKandydata = "Zuzanna";
+            kandydat[8].procMatematyka = 95;
+            kandydat[9].procInformatyka = 70;
+            kandydat[10].procJezykObcy = 85;
+
+            for (int i = 0; i < 11; i++)
+            {
+
+                if (i % 4 == 0)
+                    Console.WriteLine();
+                else
+                    Console.Write(kandydat[i]);
+
+            }
+
+        }
+    }
+}
